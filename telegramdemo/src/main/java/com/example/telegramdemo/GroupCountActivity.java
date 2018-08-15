03b2cf3f1b86b077493b08c1422290a28dbaf9cc
zsl <zsl>
2018-08-15 18:12:19 +0800

    群人数统计

--- telegramdemo/src/main/java/com/example/telegramdemo/GroupCountActivity.java
+++ telegramdemo/src/main/java/com/example/telegramdemo/GroupCountActivity.java
@@ -1,19 +1,46 @@
 package com.example.telegramdemo;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 
 import com.example.telegramdemo.util.ToastUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class GroupCountActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppCompatEditText groupLink;
     private AppCompatButton send;
+    private AppCompatTextView number;
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    number.setText(String.valueOf("该群当前群人数为: " + result));
+                    break;
+                case 1:
+                    //读取文件
+                    readInfo();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +49,7 @@ public class GroupCountActivity extends AppCompatActivity implements View.OnClic
         groupLink = findViewById(R.id.groupLink);
         send = findViewById(R.id.send);
         send.setOnClickListener(this);
+        number = findViewById(R.id.number);
     }
 
     @Override
@@ -35,6 +63,24 @@ public class GroupCountActivity extends AppCompatActivity implements View.OnClic
             //intent.putExtra("link", "hhhhhh696969");
             //发送广播
             sendBroadcast(intent);
+            handler.sendEmptyMessageDelayed(1, 5000);
+        }
+    }
+
+    private String result = "";
+    public void readInfo(){
+        File file = new File("/sdcard/groupcounts.txt");
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            result = br.readLine();
+            ToastUtil.normalShow(GroupCountActivity.this, result, true);
+            handler.sendEmptyMessage(0);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ToastUtil.normalShow(GroupCountActivity.this, "读取文件失败", true);
         }
     }
 
