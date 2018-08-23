3e5ee5f2e7a430003452df0006953b2f3976da53
zsl <zsl>
2018-08-23 16:32:50 +0800

    建群

--- telegramdemo/src/main/java/com/example/telegramdemo/CreatGroupActivity.java
+++ telegramdemo/src/main/java/com/example/telegramdemo/CreatGroupActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.example.telegramdemo.util.ToastUtil;
@@ -14,6 +15,7 @@ public class CreatGroupActivity extends AppCompatActivity {
 
     private AppCompatButton create;
     private AppCompatEditText groupName;
+    private AppCompatEditText groupLink;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class CreatGroupActivity extends AppCompatActivity {
         setContentView(R.layout.creategroup);
         create = findViewById(R.id.creat);
         groupName = findViewById(R.id.groupName);
+        groupLink = findViewById(R.id.groupLink);
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +32,10 @@ public class CreatGroupActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setAction("createGroup");
                 String name = groupName.getText().toString();
+                String link = groupLink.getText().toString();
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(link)) return;
                 intent.putExtra("groupName", name.trim());
+                intent.putExtra("groupLink", link.trim());
                 //发送广播
                 sendBroadcast(intent);
             }
